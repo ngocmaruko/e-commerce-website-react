7822d2062943f2999bc65bc0655b58feb628b544
Ngoc <ngoc.held9@gmail.com>
2024-08-09 17:03:18 +0900

    routes

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/Frontend_Assets/logo.png'
 import cart_icon from '../Assets/Frontend_Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
   
@@ -14,14 +15,14 @@ export const Navbar = () => {
         <p>SHOPPER</p>
       </div>
       <ul className="nav-menu">
-        <li onClick={() => setMenu('Shop')} >Shop {menu === 'Shop' ? <hr /> : <></>}</li>
-        <li onClick={() => setMenu('Men')} >Men{menu === 'Men' ? <hr /> : <></>}</li>
-        <li onClick={() => setMenu('Women')} >Women{menu === 'Women' ? <hr /> : <></>}</li>
-        <li onClick={() => setMenu('Kids')} >Kids{menu === 'Kids' ? <hr /> : <></>}</li>
+        <li onClick={() => setMenu('Shop')} ><Link style={{textDecoration: 'none'}} to='/'>Shop</Link> {menu === 'Shop' ? <hr /> : <></>}</li>
+        <li onClick={() => setMenu('Men')} ><Link style={{textDecoration: 'none'}} to='/mens'>Men</Link>{menu === 'Men' ? <hr /> : <></>}</li>
+        <li onClick={() => setMenu('Women')} ><Link style={{textDecoration: 'none'}} to='/womens'>Women</Link>{menu === 'Women' ? <hr /> : <></>}</li>
+        <li onClick={() => setMenu('Kids')} ><Link style={{textDecoration: 'none'}} to='/kids'>Kids</Link>{menu === 'Kids' ? <hr /> : <></>}</li>
       </ul>
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="" />
+        <Link to='/login'><button>Login</button></Link>
+        <Link to='/cart'><img src={cart_icon} alt="" /></Link>
         <div className="nav-cart-count">0</div>
       </div>
     </div>
